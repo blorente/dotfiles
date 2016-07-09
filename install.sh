216c82561b1fc5157e1930e1b024f91e010ff07c
Borja Lorente <blorente@ucm.es>
2016-07-09 17:50:39 +0200

    A bunch more files

--- install.sh
+++ install.sh
@@ -8,18 +8,26 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 EXTRA_DIR="$HOME/.extra"
 echo "Dotfiles are in ${DOTFILES_DIR}"
 
-# Update dotfiles itself first
+# Update dotfiles themselves first
 
 echo "Updating dotfiles from remote..."
 
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
-# Bunch of symlinks
+# Handle shell configurations:
+if [ "$( echo $SHELL )" == "/bin/zsh" ] then
+  echo "ZSH detected... Loading configuration"
+  echo " => Installing Oh-My-Zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  echo " => Loading configuration file..."
+  ln -sfv "$DOTFILES_DIR/runcom/zsh/.zshrc" ~
+else if [ "$( echo $SHELL )" == "/bin/zsh" ] then
+  echo "BASH detected... Loading configuration"
+  ln -sfv "$DOTFILES_DIR/runcom/bash/.bash_profile" ~
+  ln -sfv "$DOTFILES_DIR/runcom/bash/.inputrc" ~
+  ln -sfv "$DOTFILES_DIR/runcom/bash/.bashrc"~
+  ln -sfv "$DOTFILES_DIR/system/.alias" ~
+fi
 
-echo "Creating symlinks..."
 
-ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
-ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
-ln -sfv "$DOTFILES_DIR/runcom/.bashrc"~
-ln -sfv "$DOTFILES_DIR/system/.alias" ~
 #ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
