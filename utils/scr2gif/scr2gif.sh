3c3e61d6b250dfe8d8a02a8f6d2118df4958d320
Borja Lorente <blorente@ucm.es>
2016-07-10 23:02:43 +0200

    ADD dated timestamp to scr2gif out files

--- utils/scr2gif/scr2gif.sh
+++ utils/scr2gif/scr2gif.sh
@@ -4,3 +4,5 @@ ffcast -s % ffmpeg -y -f x11grab -show_region 1 -framerate 15 \
     -video_size %s -i %D+%c -codec:v huffyuv                  \
     -vf crop="iw-mod(iw\\,2):ih-mod(ih\\,2)" $TMP_AVI         \
 && convert -set delay 10 -layers Optimize $TMP_AVI out.gif
+DATE=`date +%Y-%m-%d:%H:%M:%S`
+mv out.gif $DATE.gif
