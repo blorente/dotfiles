eda41c544bbcdc32bcd705e4e39e572bb8ceef06
Borja Lorente <blorente@ucm.es>
2016-07-10 22:14:23 +0200

    Add scr2gif utility

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ echo "Updating dotfiles from remote..."
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 # Handle shell configurations:
-if [ "$( echo $SHELL )" == "/bin/zsh" ]; then
+if [ "$( echo $SHELL )" == "usr/bin/zsh" ]; then
   echo "ZSH detected... Loading configuration"
   echo " => Installing Oh-My-Zsh..."
   curl -L http://install.ohmyz.sh | sh
@@ -28,5 +28,23 @@ elif [ "$( echo $SHELL )" == "/bin/bash" ]; then
   ln -sfv "$DOTFILES_DIR/runcom/bash/.bashrc"~
   ln -sfv "$DOTFILES_DIR/system/.alias" ~
 fi
+
+# Vim plugins
+echo "Installing VIM plugins..."
+# Vundle
+echo " => Vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Copy .vimrc file
+echo " => Load .vimrc" 
+ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
+# Install Vundle plugins
+vim +PluginInstall +qall
+
+# YouCompleteMe
+#echo " => YouCompleteMe (with semantic completion)"
+#pacman -S clang
+#pacman -S cmake
+
+
 
 #ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
