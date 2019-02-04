07853527ba123f5ff0893bf50001671a6b95bb8a
Borja Lorente <bescobar@twitter.com>
2019-02-04 10:21:45 +0000

    Add vim-plug

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,4 +1,10 @@
 function setup_vim() {
+  # Install vim-plug
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  
+  # Link dotfiles
   ln -s ~/dotfiles/vim/vimrc .vimrc
-  ln -s ~/dotfiles/vim/colors ~/.vim/colors
+
+  # Run plugin installation
+  vim +PlugInstall +qall
 }
