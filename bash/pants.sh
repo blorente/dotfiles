b6b48a1e42efe4c06917676200063e85d15be124
Borja Lorente <bescobar@twitter.com>
2019-04-23 13:47:50 +0100

    Some pants aliases

--- bash/pants.sh
+++ bash/pants.sh
@@ -5,9 +5,13 @@ function pants_test() {
         relative_path=$1
         test_name=$2
 
-        target="tests/python/$(echo ${relative_path} | sed 'sX/test_.*X::X')"
-        ./pants test ${target} -- -s -k ${test_name}
+        ./pants --owner-of=tests/python/${relative_path} test -- -s -k ${test_name}
         )
 }
 
 alias pt="pants_test "
+
+function _pantsd_watch() {
+        watch -n0.3 'ps aux | grep "pantsd.* \[$( pwd )\] \| pantsd-runner \[" | grep -v watch | grep -v tail | grep -v grep'
+}
+alias pantsd_watch="_pantsd_watch"
