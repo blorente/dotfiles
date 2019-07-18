644efdee99b8a34bc2dc311fa9bbd9ce0dd21cf1
Borja Lorente <bescobar@twitter.com>
2019-07-18 10:02:04 +0100

    Maybe install python ls when we setup vim

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,4 +1,11 @@
+function install_python_ls() {
+  (set -x
+    sudo pip3 install 'python-language-server[rope]' || echo 0
+  )
+}
+
 function setup_vim() {
+  (set -xe
   # Install vim-plug
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   
@@ -7,4 +14,8 @@ function setup_vim() {
 
   # Run plugin installation
   vim +PlugInstall +qall
+
+  # Possibly install several language servers
+  install_python_ls
+  )
 }
