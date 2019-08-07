db99b61b57b55a3e039d7a049c1fc408157b8cce
Borja Lorente <bescobar@twitter.com>
2019-08-07 15:35:52 +0100

    Modify fo alias to use fzf

--- bash/header.sh
+++ bash/header.sh
@@ -1,35 +1,3 @@
-find_file () {
-	if [ $# -eq 2 ]
-  then
-  	max_depth="$2"
-	  min_depth="$2"
-	else
-		min_depth=0
-		max_depth=1000000
-	fi
-	find . -name "$1" -maxdepth "$max_depth" -mindepth "$min_depth" 
-}
-alias ffile="find_file "
-
-open_if_one () {
-    results="$1"
-    if [[ $(wc -l < <( echo "$results" ) ) -eq 1 ]]
-    then
-        emacst "$results"
-    else
-        echo "$results"
-    fi
-}
-rg_and_open () {
-    open_if_one $( rg -l "$1" )
-}
-alias rgo="rg_and_open "
-ffile_and_open () {
-    res=$( find_file "$1" )
-    echo "$res"
-    open_if_one "$res"
-}
-alias fo="ffile_and_open "
 
 # Editors
 
@@ -38,6 +6,7 @@ alias emacsc="emacsclient -c &"
 
 alias v='nvim '
 alias c='code '
+
 # Silly things
 
 alias train="while : ; do sl -a ; done"
@@ -47,6 +16,10 @@ do_timer () {
 alias timer="do_timer"
 
 export ANSIBLE_COW_SELECTION=eyes
+
+# FZF things
+
+alias fo='v $(fzf) '
 export FZF_DEFAULT_OPTS='--height 20% --layout=reverse --border'
 
 PATH="$PATH:/home/blorente/.conscript/bin"
