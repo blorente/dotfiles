52b0dc7b1bf473301e87ae5bb06a311882cd8761
Borja Lorente <bescobar@twitter.com>
2019-09-24 11:23:28 +0100

    Add Pants test from reference ptr alias

--- bash/pants.sh
+++ bash/pants.sh
@@ -11,6 +11,19 @@ function pants_test() {
 
 alias pt="pants_test "
 
+function pants_test_from_reference() {
+        (
+        set -ex
+        ref="$1"
+        # Owner file is the Python package name, with tests/python/pants_test/ prepended, and dots replaced with slashes
+        target=$(echo "$ref" | awk -F'.' '{printf "tests/python/pants_test" ; for (i=1; i<NF; i++) printf "/"$i ; print ".py"}')
+        testname=$(echo "$ref" | awk -F"#" '{print $2}')
+
+        ./pants --owner-of="$target" test -- -s -k "$testname"
+        )
+}
+alias ptr="pants_test_from_reference "
+
 function _pantsd_watch() {
         watch -n0.3 'ps aux | grep "pantsd.* \[$( pwd )\] \| pantsd-runner \[" | grep -v watch | grep -v tail | grep -v grep'
 }
