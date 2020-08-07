f72091bf3a3b65c7071c8fdd2697ea1e1ac0ef24
Borja Lorente <bescobar@twitter.com>
2020-08-07 11:05:24 +0100

    More jar stuff

--- bash/jars.sh
+++ bash/jars.sh
@@ -3,3 +3,15 @@ function peek_jar() {
   jar tvf "${jarfile}" | awk '{print $8}' | sort
 }
 
+
+function peek_jar_file() {
+  jarfile=$1
+  file_to_peek=$2
+  unzip -p "${jarfile}" "${file_to_peek}"
+}
+
+function peek_jar_manifest() {
+  jarfile=$1
+  peek_jar_file "${jarfile}" "META-INF/MANIFEST.MF"
+}
+
