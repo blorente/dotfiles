240f9e70b63b5c6f7a422fb1789661f4e2e3dbf7
Borja Lorente <blorente@apple.com>
2023-03-09 17:31:50 +0000

    Add override repo

--- aliases/bazel.sh
+++ aliases/bazel.sh
@@ -10,3 +10,31 @@ alias b="maybe_dotslash_bazel "
 alias bqb="maybe_dotslash_bazel query --output build "
 alias bb="maybe_dotslash_bazel build "
 alias bt="maybe_dotslash_bazel test "
+
+function expand_override_flag() {
+  repo_query=$1
+  repo_path=$(gop "${repo_query}")
+  repo_name=$(basename "${repo_path}")
+  echo "--override_repository=${repo_name}=${repo_path}"
+}
+alias over="expand_override_flag "
+
+function setup_ruleset_with_patches() {
+  ruleset_name=$1
+  base_ref=$2
+  patches=( "${@:3}" )
+
+  ruleset_path=$(gop "${ruleset_name}") 
+
+  cd "${ruleset_path}" || exit
+  git fetch upstream
+  git checkout "${base_ref}"
+  git checkout -b "testing_${ruleset_name}"
+
+  for patch in "${patches[@]}"; do
+    echo "$patch"
+    git apply "$patch"
+    git add .
+    git commit -m "Patch: $patch"
+  done
+}
