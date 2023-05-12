809e8d51e032548712f2ed9d747a2b59fcd60aaa
Borja Lorente <blorente@apple.com>
2023-05-12 10:31:05 +0100

    Cleanup

--- aliases/common.sh
+++ aliases/common.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-function banner() {
-  msg="$1"
-  echo "[=== ${msg} ===]"
-}
-
-function github_clone() {
-  repo="$1"
-  dest="~/github/${repo}"
-  git clone "https://github.com/${repo}" "${dest}" 
-  banner "Cloned ${repo}"
-}
-
 alias cppwd="readlink -f $(pwd) | tr -d ' ' | tr -d '\r' | pbcopy"
 alias cpwd="readlink -f $(pwd) | tr -d ' ' | tr -d '\r' | pbcopy"
 function cpabs() {
