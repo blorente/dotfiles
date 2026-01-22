3ff4478760bc3b57d168be49db47f9ce64d56d9c
Borja Lorente <blorente.me@gmail.com>
2026-01-22 19:45:09 +0000

    fix(nvim): Enable <leader>y for yanking to system clipboard

--- configs/nvim/lua/blorente/init.lua
+++ configs/nvim/lua/blorente/init.lua
@@ -2,6 +2,8 @@
 -- Remap the leader keys before loading lazy, otherwise they will not be set correctly when we load plugins
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+vim.opt.clipboard = vim.env.SSH_CONNECTION and "" or "unnamedplus" -- Sync with system clipboard
+
 
 require("blorente.lazy")
 require("blorente.remaps")
